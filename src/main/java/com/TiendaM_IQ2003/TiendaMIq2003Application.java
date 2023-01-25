6c1163bc0bc4b81945101769dc81b71428fb1f27
Daniel Roda <Daniel Roda@DESKTOP-48GF030>
2023-01-24 23:55:10 -0600

    Creación del proyecto inicial

---
+++ src/main/java/com/TiendaM_IQ2003/TiendaMIq2003Application.java
@@ -0,0 +1,13 @@
+package com.TiendaM_IQ2003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMIq2003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMIq2003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaM_IQ2003/TiendaMIq2003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaM_IQ2003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMIq2003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
